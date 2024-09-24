9d52364e0e567b6c159d4dcebe77cdc5e5a09f52
OlgaShults <159514924+OlgaShults@users.noreply.github.com>
2024-09-24 11:42:07 +0300

    Add files via upload

--- JS II.js
+++ JS II.js
@@ -1,15 +1,15 @@
 $(document).ready(function() {
     $("#intro h3").delay(1000).queue(function(n){
-        $(this).toggleClass("fuller")
-        n()
+        $(this).toggleClass("fuller");
+        n();
     });
   
     $("#intro a").delay(1400).queue(function(n){
-        $(this).toggleClass("fuller2")
-        n()
+        $(this).toggleClass("fuller2");
+        n();
     });
 
-    $("#menu-button").click(function () {
+    $("#menu-button").click(function() {
         $("#intro h3").toggleClass("fuller");
         $("#intro a").toggleClass('fuller2');
         $("#menu-button").toggleClass("animate");
