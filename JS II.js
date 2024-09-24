4aef521ee34b28b3c6e5ac6515740897253b8daa
OlgaShults <159514924+OlgaShults@users.noreply.github.com>
2024-09-24 11:34:24 +0300

    Add files via upload

---
+++ JS II.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    $("#intro h3").delay(1000).queue(function(n){
+        $(this).toggleClass("fuller")
+        n()
+    });
+  
+    $("#intro a").delay(1400).queue(function(n){
+        $(this).toggleClass("fuller2")
+        n()
+    });
+
+    $("#menu-button").click(function () {
+        $("#intro h3").toggleClass("fuller");
+        $("#intro a").toggleClass('fuller2');
+        $("#menu-button").toggleClass("animate");
+        $("#menu-overlay").toggleClass("reveal");
+        $("#nav-container").toggleClass("hidden");
+    });
+});
